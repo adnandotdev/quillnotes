798fdac23947d5c7d7538cfd22cc45d0af79d9ca
Mohammad Adnan <mohammadadnan6460@gmail,com>
2024-09-21 20:17:29 +0530

    amon

--- models/user.js
+++ models/user.js
@@ -1,14 +1,6 @@
 const mongoose = require('mongoose')
 
-// mongoose.connect(`mongodb://localhost:27017/sample`)
-mongoose.connect(`mongodb://localhost:27017/sample`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(() => {
-    console.log('Connected to MongoDB');
-  }).catch(err => {
-    console.error('Failed to connect to MongoDB', err);
-  });
+mongoose.connect(`mongodb://localhost:27017/sample`)
 
 const userSchema = mongoose.Schema({
     name: String,
