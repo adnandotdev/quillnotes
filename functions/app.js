78224bdb374b91944f64ebcde8c89dad43e39278
Mohammad Adnan <mohammadadnan6460@gmail,com>
2024-09-23 17:08:00 +0530

    netlify to deployed-n

--- functions/app.js
+++ functions/app.js
@@ -10,6 +10,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '../views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
